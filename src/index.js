9af91b69b4ffee319899b3d084a84dd5893f4dd3
jonasbastosj <jonasbastos.j@gmail.com>
2025-09-19 09:51:17 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App"; // Certifique-se que App.js existe
 
-import "./index.css"; // opcional, se tiver CSS global
+//import "./index.css"; // opcional, se tiver CSS global
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
